dbc44a253755e94c7f131bf6d72bec0996930e7f
Myasnikov <Ymyasnikov@psi.loc>
2022-07-26 12:40:36 +0300

    Switching to the login page done (two ways)

--- pages/login_page.py
+++ pages/login_page.py
@@ -11,7 +11,7 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert "puta" in self.browser.current_url, "current url doesn't contain login"
+        assert "login" in self.browser.current_url, "current url doesn't contain login"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
--- test_main_page.py
+++ test_main_page.py
@@ -12,10 +12,10 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-def test_guest_should_be_on_login_page(browser): 
+def test_guest_should_be_on_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-    page = LoginPage(browser, link)          
-    page.should_be_login_page()
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url) #еще один вариант перехода на новую страницу, теперь уже явно
+    login_page.should_be_login_page()
