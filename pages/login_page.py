80a99f0b6a9ecaa59c2d25a44350449a437ea5e7
Myasnikov <Ymyasnikov@psi.loc>
2022-07-26 12:27:13 +0300

    LoginPageLocators and login_page added

---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from .base_page import BasePage
+from pages.locators import LoginPageLocators
+from selenium.webdriver.common.by import By
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "puta" in self.browser.current_url, "current url doesn't contain login"
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not presented"
