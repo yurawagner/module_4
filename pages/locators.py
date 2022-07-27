c16acc6a915062e664715c18bcf9ef8e2a9af4eb
Myasnikov <Ymyasnikov@psi.loc>
2022-07-27 10:52:18 +0300

    User registration added

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,7 @@ class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_LINK = (By.CSS_SELECTOR, "div.basket-mini span a")
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
--- pages/login_page.py
+++ pages/login_page.py
@@ -1,7 +1,7 @@
 from .base_page import BasePage
 from pages.locators import LoginPageLocators
 from selenium.webdriver.common.by import By
-
+import time
 
 class LoginPage(BasePage):
     def should_be_login_page(self):
@@ -18,3 +18,16 @@ class LoginPage(BasePage):
 
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form is not presented"
+    
+    def register_new_user(self, email, password):
+        em = self.browser.find_element(By.ID, "id_registration-email")
+        em.click()
+        em.send_keys(email)
+        ps = self.browser.find_element(By.ID, "id_registration-password1")
+        ps.click()
+        ps.send_keys(password)
+        pss = self.browser.find_element(By.ID, "id_registration-password2")
+        pss.click()
+        pss.send_keys(password)
+        regbut = self.browser.find_element(By.NAME, "registration_submit")
+        regbut.click()
